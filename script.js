612ec09ea6c052e7716dd5c33269690bede7972a
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-07-12 22:41:08 -0300

    feat: add numero e nome nas variaveis

---
+++ script.js
@@ -0,0 +1,6 @@
+var nome_contato = window.document.getElementById ('nome_contato')
+var numero_contato = window.document.getElementById ('numero_contato')
+
+function Add_numero(){
+    window.alert (`${nome_contato.value} e ${numero_contato.value}`)
+}
