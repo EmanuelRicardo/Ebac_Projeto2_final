c6078aaa5ed6508c2b4b667600f153280cac6a72
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-07-14 14:44:51 -0300

    feat: contador de contatos add

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 var nome_contato = window.document.getElementById ('nome_contato')
 var numero_contato = window.document.getElementById ('numero_contato')
 var linhas = '' 
-var total_contatos = ''
 var escrever_total_contatos = window.document.getElementById ('total_contatos_add')
+var contador_de_contatos = 0
+
 
 
 
 function Add_numero(event){
     event.preventDefault();
-    var linha = '<tr>'
-    linha += `<td>${nome_contato.value}</td>`
-    linha += `<td>${numero_contato.value}</td>`
-    linha += `</tr>`
-
-    linhas += linha
+    if (nome_contato.value.length == 0 || numero_contato.value.length == 0){
+        window.alert ("Tá faltando informações, verifique o número e nome e tente novamente")
+    } else{
+        var linha = '<tr>'
+        linha += `<td>${nome_contato.value}</td>`
+        linha += `<td>${numero_contato.value}</td>`
+        linha += `</tr>`
+        linhas += linha
+        contador_de_contatos += 1
+        escrever_total_contatos.innerHTML = contador_de_contatos
+    }
+    
 
     var corpo_tabela = window.document.getElementById ('corpo_tabela')
     corpo_tabela.innerHTML = linhas
