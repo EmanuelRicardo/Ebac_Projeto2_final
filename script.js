3e9051d8b2e92fe22aaefbec8124491c65373ccb
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-07-13 22:35:27 -0300

    feat: function add nome e numero na pag

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 var nome_contato = window.document.getElementById ('nome_contato')
 var numero_contato = window.document.getElementById ('numero_contato')
+var linhas = '' 
 
-function Add_numero(){
-    window.alert (`${nome_contato.value} e ${numero_contato.value}`)
+function Add_numero(event){
+    event.preventDefault();
+    var linha = '<tr>'
+    linha += `<td>${nome_contato.value}</td>`
+    linha += `<td>${numero_contato.value}</td>`
+    linha += `</tr>`
+
+    linhas += linha
+
+    var corpo_tabela = window.document.getElementById ('corpo_tabela')
+    corpo_tabela.innerHTML = linhas
 }
